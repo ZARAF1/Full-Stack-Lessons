fa9d1c76c23dfb3d457156969585ec83dc457a4c
zaraf1 <ali.raffad@gmail.com>
2023-10-17 11:52:36 +0300

    Char code at and char examples modified and included

--- 9. JavaScript Syntax, Part I/1. Introduction/1. Lesson 1/1. introduction to JS.js
+++ 9. JavaScript Syntax, Part I/1. Introduction/1. Lesson 1/1. introduction to JS.js
@@ -15,6 +15,8 @@ Multi line comment*/
 // object
 // */
 
+//const logBlacklistError = require('gulp-cli/lib/shared/log/blacklist-error');
+
 /* string properties and methods*/
 const str = 'hello i am a string';
 
@@ -23,32 +25,45 @@ const stringsLength = str.length;
 console.log('Length of the string is ' + stringsLength);
 console.log('Length of another string is ' + 'this is another string'.length);
 
-// uppercase
+// operation being stored in a variable
+const upperText = str.toUpperCase();
+console.log(upperText + '<<<< converted to uppercase');
+
+// uppercase on the variable holding the string
 
 console.log(str.toUpperCase());
 console.log(str.startsWith('h'));
 console.log(str.length);
 
-//another way directly on the string
+// another way directly on the string
 
 console.log('hello'.toUpperCase());
 console.log('hello'.startsWith('h'));
 
-//builtin objects
+// builtin objects
+
 console.log(Math.floor(Math.random() * 50));
 let storeVar = Math.floor(Math.random() * 5);
 console.log(storeVar);
 // method to remove white spaces from the beginning and end of text
 console.log(
-  '        This will remove whitespaces from the frucking text      '.trim()
+  '        This will remove whitespaces from the freakish text      '.trim()
 );
 
-// character code at
+// The charCodeAt() method of String values returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
 console.log('Hello World'.charCodeAt(1));
 
-const newArry = [1, 23, 2, 4, 5];
-console.log(newArry.length);
+const index = 3;
+const txt = 'Hello World';
+console.log(
+  `The character at index: ${index} is ${txt.charAt(
+    index
+  )} while the character code for it is ${txt.charCodeAt(index)}`
+);
 
-newArry.forEach(element => {
+const newArray = [1, 23, 2, 4, 5];
+console.log(newArray.length);
+
+newArray.forEach(element => {
   console.log(element);
 });
