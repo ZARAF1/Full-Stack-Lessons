5b47c6d50967ceac4ab68554f9adcea6f667a39b
zaraf1 <ali.raffad@gmail.com>
2023-10-17 13:34:55 +0300

    Renamed string to an array

--- 9. JavaScript Syntax, Part I/1. Introduction/1. Lesson 1/revision.js
+++ 9. JavaScript Syntax, Part I/1. Introduction/1. Lesson 1/revision.js
@@ -1,25 +1,23 @@
-const test = "i am a string";
-const randomNumber = Math.floor(Math.random() * 5)
-console.log(test)
+const test = 'i am a string';
+const randomNumber = Math.floor(Math.random() * 5);
+console.log(test);
 console.log(test.length);
-console.log(randomNumber
-)
+console.log(randomNumber);
 
-console.log(test.startsWith("i"))
-console.log("improvised".startsWith("i"))
-console.log(Math.random() * 5)
-
+console.log(test.startsWith('i'));
+console.log('improvised'.startsWith('i'));
+console.log(Math.random() * 5);
 
 // trim method
 
-let text = "  He llo ";
-let newString = []
+let text = '  He llo ';
+let newStringArray = [];
 for (let char of text) {
-    if (char === " ") {
-        continue;
-    } else {
-        newString.push(char)
-    }
+  if (char === ' ') {
+    continue;
+  } else {
+    newStringArray.push(char);
+  }
 }
 
-console.log(newString)
+console.log(newStringArray);
