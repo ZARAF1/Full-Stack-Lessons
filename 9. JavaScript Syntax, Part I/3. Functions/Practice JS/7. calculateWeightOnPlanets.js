73c6e93e4359dfcfc11ec40040021fed604b4e0a
zaraf1 <ali.raffad@gmail.com>
2023-10-18 16:35:05 +0300

    another solution provided converted if else to switch case

--- 9. JavaScript Syntax, Part I/3. Functions/Practice JS/7. calculateWeightOnPlanets.js
+++ 9. JavaScript Syntax, Part I/3. Functions/Practice JS/7. calculateWeightOnPlanets.js
@@ -15,20 +15,41 @@ Handle the following cases:
 'Saturn' weight = earthWeight * 0.916
 For all other inputs, return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
 * */
-
+// solution 1
 const calculateWeight = (earthWeight, planet) => {
-    if (planet === "Mercury") {
-        return earthWeight * 0.378;
-    } else if (planet === "Venus") {
-        return earthWeight * 0.907;
-    } else if (planet === 'Mars') {
-        return earthWeight * 0.377;
-    } else if (planet === "Jupiter") {
-        return earthWeight * 2.36;
-    } else if (planet === "Saturn") {
-        return earthWeight * 0.916;
-    } else {
-        return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
-    }
-}
-console.log(calculateWeight(65, "Saturn"))
+  if (planet === 'Mercury') {
+    return earthWeight * 0.378;
+  } else if (planet === 'Venus') {
+    return earthWeight * 0.907;
+  } else if (planet === 'Mars') {
+    return earthWeight * 0.377;
+  } else if (planet === 'Jupiter') {
+    return earthWeight * 2.36;
+  } else if (planet === 'Saturn') {
+    return earthWeight * 0.916;
+  } else {
+    return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+  }
+};
+console.log(calculateWeight(65, 'Mercury'));
+
+// solution 2 using switch case
+const calcWeight = (earthWeight, planet) => {
+  let weight;
+  switch (planet) {
+    case 'Mercury':
+      weight = earthWeight * 0.378;
+      break;
+    case 'Venus':
+      weight = earthWeight * 0.907;
+      break;
+    case 'Mars':
+      weight = earthWeight * 0.377;
+      break;
+    default:
+      return `Unmatched`;
+  }
+  return weight;
+};
+console.log(calculateWeight(65, 'Mercury'));
+console.log(calcWeight(65, 'Mercury') + '<<<');
