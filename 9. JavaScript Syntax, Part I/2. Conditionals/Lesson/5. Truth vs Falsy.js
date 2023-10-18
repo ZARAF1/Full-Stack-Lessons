3e879b8f3e2ff3a52320165582826e0c816b25c8
zaraf1 <ali.raffad@gmail.com>
2023-10-18 12:25:09 +0300

    Truth vs falsy examples updated

--- 9. JavaScript Syntax, Part I/2. Conditionals/Lesson/5. Truth vs Falsy.js
+++ 9. JavaScript Syntax, Part I/2. Conditionals/Lesson/5. Truth vs Falsy.js
@@ -1,22 +1,23 @@
-// please note that truth vs falsy is different than true and false because truthy specifies whether a variable
-// has been assigned a value
+// please note that truth vs falsy is different than true and false because truthy specifies whether a variable has been assigned a value
 
 /** truth and falsy values **/
 // NaN, null, undefined, "", 0, false will also result in Falsy statements
-let var1 = NaN;
+let var1 = '';
 if (var1) {
   console.log('truthy');
 } else {
   console.log('falsy');
 }
-console.log(2 || 3);
-let val = 0 || (2 && 3); // 0 false undefined "" will also result in false shortcut evaluation
+
+/** Shortcut evaluation when using logical operators */
+console.log(2 || 3); //when JavaScript evaluates an OR expression, if the first operand is true, JavaScript will not even look at the second operand in the short hand evaluation. So its 2
+let val = 0 || (2 && 3); // 3  // 0, false, undefined, "", NAN, null will also result in false shortcut evaluation
 console.log(val + ' is the value of val!');
-// but then this might confuse you
+// but then this might confuse you but dont be because truth vs falsy is different than true and false because truthy specifies whether a variable has been assigned a value
 console.log(1 == true);
 console.log(1 === true);
 
-val === 'number'
+typeof val === 'number'
   ? console.log('number is true')
   : console.log('number is false');
 
@@ -25,7 +26,7 @@ let defaultName = '';
 let name = defaultName || 'stranger';
 console.log(name);
 
-let defaulterName = 'Ali';
+let defaulterName = '';
 let namee;
 if (defaulterName) {
   namee = defaulterName;
@@ -34,3 +35,10 @@ if (defaulterName) {
   namee = 'Stranger';
   console.log(namee);
 }
+
+var person = {
+  name: 'Jack',
+  age: 34,
+};
+console.log(person.job); // undefined
+console.log(person.job || 'unemployed'); // 'unemployed'
