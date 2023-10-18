c17ba1e5a1e68324c8f20d532ac23e84ca7d3f92
zaraf1 <ali.raffad@gmail.com>
2023-10-18 14:38:43 +0300

    Updated can i vote exercise with new changes!

--- 9. JavaScript Syntax, Part I/3. Functions/Practice JS/1. canIVote.js
+++ 9. JavaScript Syntax, Part I/3. Functions/Practice JS/1. canIVote.js
@@ -1,16 +1,19 @@
 /*
-The most common minimum age to vote is 18. Write a function canIVote() that takes in a number, representing
-the person’s age, and returns the boolean true if they are 18 years old or older, and the boolean false if they are not.
+The most common minimum age to vote is 18. Write a function canIVote() that takes in a number, representing the person’s age, and returns the boolean true if they are 18 years old or older, and the boolean false if they are not.
 * */
 
-const canIVote=(age)=>{
-    if (typeof age==="number" && age >= 18){
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+const canIVote = age => {
+  if (typeof age !== 'number') {
+    return 'please enter a number!';
+  } else if (typeof age === 'number' && age < 18) {
+    return `You are too young to vote please wait ${18 - age} ${
+      18 - age > 1 ? 'years' : 'year'
+    }`;
+  } else if (typeof age === 'number' && age >= 18) {
+    return `Welcome to vote!`;
+  } else {
+    return `Must have committed a sin to reach here!`;
+  }
+};
 
-console.log(canIVote(17))
+console.log(canIVote(15));
