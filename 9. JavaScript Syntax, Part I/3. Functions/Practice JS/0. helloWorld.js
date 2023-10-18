d48802431e89b48b9b5a11e8633a655312ba1630
zaraf1 <ali.raffad@gmail.com>
2023-10-18 14:08:57 +0300

    Added different type of function declarations for the exercise

--- 9. JavaScript Syntax, Part I/3. Functions/Practice JS/0. helloWorld.js
+++ 9. JavaScript Syntax, Part I/3. Functions/Practice JS/0. helloWorld.js
@@ -1,6 +1,17 @@
-const greetWorld=()=>{
-    return 'Hello, World!'
+// function declaration
+function greetThyWorld() {
+  console.log('greetings aliens living on earth');
+}
+// function expressions
+const greetMyWorld = function (entity){
+    console.log(`I return to greet the world of humanoids!`);
 }
 
-console.log(greetWorld())
+// Arrow functions 
+const greetWorld = () => {
+  return 'Hello, World!';
+};
+console.log(greetWorld());
 
+// concise arrow functions 
+const 
