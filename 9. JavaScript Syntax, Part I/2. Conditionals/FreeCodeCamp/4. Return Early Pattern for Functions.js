3482c33d54585ef72113326c50261d9b452ca54b
zaraf1 <ali.raffad@gmail.com>
2023-10-18 13:17:23 +0300

    Example updated

--- 9. JavaScript Syntax, Part I/2. Conditionals/FreeCodeCamp/4. Return Early Pattern for Functions.js
+++ 9. JavaScript Syntax, Part I/2. Conditionals/FreeCodeCamp/4. Return Early Pattern for Functions.js
@@ -22,16 +22,16 @@ Remember that undefined is a keyword, not a string.
 
 // Setup
 function abTest(a, b) {
-    // Only change code below this line
-    if(a<0 || b<0){
-        return undefined;
-    }
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
 
+  // Only change code above this line
 
-    // Only change code above this line
-
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-console.log(abTest(2,2));
-console.log(Math.sqrt(2))
+console.log(abTest(2, 2));
+console.log(Math.sqrt(2));
+console.log(Math.round(7.92));
