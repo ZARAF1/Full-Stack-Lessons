41f29f85f60b44f407f49d69738d1a7e06b26a3c
zaraf1 <ali.raffad@gmail.com>
2023-10-19 18:09:28 +0300

    Array methods updated

--- 10. JavaScript Syntax, Part II/1. Arrays/Lesson/8. More Array Methods.js
+++ 10. JavaScript Syntax, Part II/1. Arrays/Lesson/8. More Array Methods.js
@@ -1,26 +1,25 @@
 const groceryList = [
-  "orange juice",
-  "bananas",
-  "coffee beans",
-  "brown rice",
-  "pasta",
-  "coconut oil",
-  "plantains",
+  'orange juice',
+  'bananas',
+  'coffee beans',
+  'brown rice',
+  'pasta',
+  'coconut oil',
+  'plantains',
 ];
 groceryList.shift(); // removes first element
 console.log(groceryList);
 let shiftedElement = groceryList.shift();
 console.log(shiftedElement);
 
-groceryList.unshift("tea", "brocolli"); // adds first element / elements
+groceryList.unshift('tea', 'brocolli'); // adds first element / elements
 
 console.log(groceryList);
 
-/*
-console.log(groceryList.slice(1,4))
-console.log(groceryList)
-const pastaIndex=groceryList.indexOf('pasta');
-console.log(pastaIndex)*/
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
 
 const slicedArray = groceryList.slice(0, 2);
 console.log(slicedArray);
