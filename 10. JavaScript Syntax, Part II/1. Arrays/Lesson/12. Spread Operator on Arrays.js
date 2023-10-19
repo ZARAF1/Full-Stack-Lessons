490bc2de1cddc7b5d2d72a850d97d9f67e0e317a
zaraf1 <ali.raffad@gmail.com>
2023-10-19 18:11:05 +0300

    Added an example

--- 10. JavaScript Syntax, Part II/1. Arrays/Lesson/12. Spread Operator on Arrays.js
+++ 10. JavaScript Syntax, Part II/1. Arrays/Lesson/12. Spread Operator on Arrays.js
@@ -1,22 +1,22 @@
 // declare two arrays and a string to understand spread
 
-const arr = [1, 2, 3, 4]
-const arr2 = [5, 6, 7, 8, 9]
+const arr = [1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8, 9];
 const word = 'test data';
 
-
 /*The spread operator unpacks an array into separate arguments:*/
-console.log("Unpack an array into separate arguments: ")
-console.log(...arr) // unpacking single array
-console.log(...arr, ...arr2)
-console.log([...arr, ...arr2])
-
+console.log('Unpack an array into separate arguments: ');
+console.log(...arr); // unpacking single array
+console.log(...arr, ...arr2);
+console.log([...arr, ...arr2]);
+console.log([...word]);
 
 /*These operations are not only available for arrays but also other iterables like strings:*/
-console.log("These operations are not only available for arrays but also other iterables like strings:")
+console.log(
+  'These operations are not only available for arrays but also other iterables like strings:'
+);
 console.log(...word);
-console.log([...word])
-
+console.log([...word]);
 
 /*To copy/clone an array*/
 
@@ -27,32 +27,30 @@ console.log([...word])
 const originalArr = [10, 11, 12, 13, 14, 15, 16];
 const copiedArr = originalArr;
 
-copiedArr[0] = 556
-console.log({copiedArr})
-console.log({originalArr}) //let copiedArr = originalArr; changes the original array as well
+copiedArr[0] = 556;
+console.log({ copiedArr });
+console.log({ originalArr }); //let copiedArr = originalArr; changes the original array as well
 
 // Copy/ Cloning examples
 //example 1
-const copiedArray = [...arr]
-console.log("We can copy arrays like")
-console.log("Copied array is :")
-console.log(copiedArray)
+const copiedArray = [...arr];
+console.log('We can copy arrays like');
+console.log('Copied array is :');
+console.log(copiedArray);
 
 //example 2
-console.log("using spread to copy two arrays in one array")
-const newArray = [...arr, ...arr2]
-console.log(newArray)
+console.log('using spread to copy two arrays in one array');
+const newArray = [...arr, ...arr2];
+console.log(newArray);
 
 // example 3 cloning with other values
-const worded = "Hello"
+const worded = 'Hello';
 const clonedArray = [0, ...arr, 5, ...worded];
-console.log("Cloning an array:")
+console.log('Cloning an array:');
 console.log(clonedArray);
 
 /*******
  * Using spread operator also comes in handy when a function expects a list of arguments and all you have is an array
  */
 
-console.log(Math.max(...arr2))
-
-
+console.log(Math.max(...arr2));
