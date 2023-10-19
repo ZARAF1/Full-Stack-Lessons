f99e34db5348b5402a591d82d9778df27867f39c
zaraf1 <ali.raffad@gmail.com>
2023-10-19 18:10:38 +0300

    Added an example

--- 10. JavaScript Syntax, Part II/1. Arrays/Lesson/11. Array Destruction.js
+++ 10. JavaScript Syntax, Part II/1. Arrays/Lesson/11. Array Destruction.js
@@ -1,7 +1,12 @@
-const vehicles = ["mustang", "f-150", "expedition"];
+const vehicles = ['mustang', 'f-150', 'expedition'];
 const [car, truck, suv] = vehicles;
 console.log(suv);
 console.log(truck);
+
+const testArray = ['a', 'b', 1, 2];
+const [arrE1, arrE2, arrE3, arrE4] = testArray;
+console.log(arrE1, arrE2);
+console.log(testArray);
 
 // array destruction is best when used in functions
 
@@ -16,3 +21,14 @@ console.log(calculator(10, 2)); // returns an [ 12, 8, 5, 100 ]
 
 const [adder, subtract, divide, power] = calculator(10, 2); // we destruct the returned array
 console.log(adder, subtract, divide, power);
+
+// Solution 2
+
+const testCalc = (val1, val2) => {
+  let add = val1 + val2;
+  let sub = val1 - val2;
+  return [add, sub];
+};
+
+let [addition, subtraction] = testCalc(10, 2);
+console.log(addition, subtraction);
