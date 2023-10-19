1b800ab048a31b6f9f588e77087104fd39fa73e8
zaraf1 <ali.raffad@gmail.com>
2023-10-19 10:51:54 +0300

    Included multidimensional array using array contructor

--- 10. JavaScript Syntax, Part II/1. Arrays/Lesson/1. creating arrays.js
+++ 10. JavaScript Syntax, Part II/1. Arrays/Lesson/1. creating arrays.js
@@ -5,6 +5,8 @@ console.log(hobbies);
 // creating arrays using array constructor
 const newArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 console.log(newArray);
-
-const multiValueArray = new Array('a', 'b', 1, 2, 3);
-console.log(multiValueArray);
+const anotherNewArray = new Array(123, 456, 789);
+console.log(anotherNewArray);
+const multiValueMultiDimensionalArray = new Array('a', 'b', [1, 2, 3]);
+console.log(multiValueMultiDimensionalArray);
+console.log(multiValueMultiDimensionalArray[2].length);
