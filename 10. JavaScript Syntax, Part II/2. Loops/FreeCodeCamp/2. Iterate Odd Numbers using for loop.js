75a97e7d6c90dae0864d94b01baf821d49def83f
zaraf1 <ali.raffad@gmail.com>
2023-10-19 20:38:04 +0300

    Odd array solution added

--- 10. JavaScript Syntax, Part II/2. Loops/FreeCodeCamp/2. Iterate Odd Numbers using for loop.js
+++ 10. JavaScript Syntax, Part II/2. Loops/FreeCodeCamp/2. Iterate Odd Numbers using for loop.js
@@ -3,19 +3,27 @@ For loops don't have to iterate one at a time. By changing our final-expression,
 
 We'll start at i = 0 and loop while i < 10. We'll increment i by 2 each loop with i += 2.*/
 
-    var ourArray = [];
+var ourArray = [];
 for (var i = 0; i < 10; i += 2) {
-    ourArray.push(i);
+  ourArray.push(i);
 }
 // ourArray will now contain [0,2,4,6,8]. Let's change our initialization so we can count by odd numbers.
-console.log(ourArray)
+console.log(ourArray);
 // Another example
 
-let arr=[0,1,2,3,4,5,6,7,8,9,10];
-let evenArray=[];
-for (let i=0;i<arr.length;i++){
-    if(arr[i]%2===0){
-        evenArray.push(arr[i])
-    }
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let evenArray = [];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    evenArray.push(arr[i]);
+  }
 }
-console.log(evenArray)
+console.log(evenArray);
+
+let oddArray = [];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 1) {
+    oddArray.push(arr[i]);
+  }
+}
+console.log(oddArray);
