4cee6484b16c741dbf6fdd052c158885049fac39
zaraf1 <ali.raffad@gmail.com>
2023-10-21 18:21:29 +0300

    Examples updated

--- 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/2. 10.2 More Built In Methods/3. Object values.js
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/2. 10.2 More Built In Methods/3. Object values.js
@@ -1,31 +1,34 @@
-//  takes the key value pair and stores it in an array if nested object exists then it takes the nested object and stores it
-// in the array
+//  takes the prop values and stores it in an array if nested object exists then it takes the nested object and stores it in the array
 
 let student = {
-    name: 'Egill Vignission',
-    gpa: 3.4
-}
-console.log(Object.values(student))
-
+  name: 'Egill Vignission',
+  gpa: 3.4,
+  intro() {
+    return `${this.name} has a gpa of ${this.gpa}`;
+  },
+};
+console.log(Object.values(student));
 
 const GPA_BENCHMARK = 3.5;
 let students = {
-    1: {
-        name: 'Egill Vignission',
-        gpa: 3.4
-    },
-    2: {
-        name: 'Bianca Pargas',
-        gpa: 3.8
-    },
-    3: {
-        name: 'Aisling O\'Sullivan',
-        gpa: 3.4
-    },
-    4: {
-        name: 'Sameer Fares',
-        gpa: 3.9
-    }
-}
-let honorRoll = Object.values(students)
-console.log(honorRoll)
+  studentCount: 4,
+  school: 'Beacon House',
+  1: {
+    name: 'Egill Vignission',
+    gpa: 3.4,
+  },
+  2: {
+    name: 'Bianca Pargas',
+    gpa: 3.8,
+  },
+  3: {
+    name: "Aisling O'Sullivan",
+    gpa: 3.4,
+  },
+  4: {
+    name: 'Sameer Fares',
+    gpa: 3.9,
+  },
+};
+let honorRoll = Object.values(students);
+console.log(honorRoll);
