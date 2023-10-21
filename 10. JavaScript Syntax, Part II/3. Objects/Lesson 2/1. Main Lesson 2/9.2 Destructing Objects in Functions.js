c33d9598bc7503e5d7e3f336ca0911fe5d80c67a
zaraf1 <ali.raffad@gmail.com>
2023-10-21 14:56:59 +0300

    Added array destruction in functions

--- 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/9.2 Destructing Objects in Functions.js
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/9.2 Destructing Objects in Functions.js
@@ -1,13 +1,22 @@
 const vehicleOne = {
-    brand: 'Ford',
-    model: 'Mustang',
-    type: 'car',
-    year: 2021,
-    color: 'red'
-}
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red',
+};
+// in objects
+const printModel = ({ brand, model, type, year }) => {
+  console.log(
+    `I have a ${type} which is of the brand ${brand} and the model is ${model} from the year ${year}`
+  );
+};
 
-const printModel=({brand,model,type,year})=>{
-    console.log(`I have a ${type} which is of the brand ${brand} and the model is ${model} from the year ${year}`)
-}
+printModel(vehicleOne);
 
-printModel(vehicleOne)
+// in arrays
+const arr = [1, 2, 3];
+const printArray = ([a, b, c]) => {
+  console.log(a, b, c);
+};
+printArray(arr);
