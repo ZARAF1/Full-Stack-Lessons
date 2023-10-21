da9178e0f1f51d17f4b5027295382757712ce169
zaraf1 <ali.raffad@gmail.com>
2023-10-21 14:51:59 +0300

    constructor function updated

--- 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/7.2 Constructor Function equal to Factory Function.js
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/7.2 Constructor Function equal to Factory Function.js
@@ -1,15 +1,22 @@
 // constructor functions
 function BellBoy(name, age, hasWorkPermit, languages) {
-    this.name = name;
-    this.age = age;
-    this.hasWorkPermit = hasWorkPermit;
-    this.languages = languages;
-    this.moveSuitCase = function () {
-        // alert("Ready to move it now!"
-        console.log("Am ready to move it now")
-    }
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+  this.moveSuitCase = function () {
+    // alert("Ready to move it now!"
+    console.log(
+      `Am ready to ${
+        this.hasWorkPermit === true ? 'work' : 'look for job'
+      } now because my name is ${this.name} and i have ${
+        this.hasWorkPermit === true ? 'a work permit' : 'no work permit'
+      }`
+    );
+  };
 }
 
-let newBellBoy1 = new BellBoy("timmy", 18, true, ["arabic", "urdu"])
-console.log(newBellBoy1)
-newBellBoy1.moveSuitCase()
+let newBellBoy1 = new BellBoy('timmy', 18, false, ['arabic', 'urdu']);
+newBellBoy1.moveSuitCase();
+console.log(newBellBoy1);
+newBellBoy1.moveSuitCase();
