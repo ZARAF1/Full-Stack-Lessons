4c730c3ff44811ca52a64e3e28c038c906b6efc3
zaraf1 <ali.raffad@gmail.com>
2023-10-22 02:33:59 +0300

    Creating objects prototypes added to add properties to object constructor functions

--- 10. JavaScript Syntax, Part II/3. Objects/Lesson 1/1. Creating Objects.js
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 1/1. Creating Objects.js
@@ -30,7 +30,11 @@ person.eyeColor = 'blue';
 
 console.log({ person });
 
-// 4: Using a constructor function
+// 4: Using an object constructor function
+// Sometimes we need a "blueprint" for creating many objects of the same "type".
+// The way to create an "object type", is to use an object constructor function.
+// In the example above, function Person() is an object constructor function.
+// Objects of the same type are created by calling the constructor function with the new keyword:
 // Constructor function for Person objects
 
 function Person(first, last, age, eye) {
@@ -39,14 +43,15 @@ function Person(first, last, age, eye) {
   this.age = age;
   this.eyeColor = eye;
 }
-// constructor function for Animal objects
-function Animal(type, first, last, age, eye) {
+
+// constructor function for Animal objects using function expressions
+const Animal = function (type, first, last, age, eye) {
   this.type = type;
   this.firstName = first;
   this.lastName = last;
   this.age = age;
   this.eyeColor = eye;
-}
+};
 
 /**** arrow function syntax doesn't work */
 // const Bird = (type, first, last, age, eye) => {
@@ -57,24 +62,27 @@ function Animal(type, first, last, age, eye) {
 //   this.eyeColor = eye;
 // };
 
-// function expressions do work
-const Bird = function (type, first, last, age, eye) {
-  this.type = type;
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
-};
-
 // Create a Person object
 const myFather = new Person('John', 'Doe', 50, 'blue');
 console.log(myFather);
 console.log(myFather.firstName);
 
+// NOTE: you can not add a new property to an existing object constructor. To add a new property to a constructor, you must add it to the constructor function see below commented out code or use object prototypes refer to next 1.1 exercise
+
+Person.nationality = 'English';
+console.log(myFather.nationality); // prints undefined
+
+/**
+ function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.nationality = "English";
+}
+ * 
+ */
+
 // Create a Animal object
 const brusoe = new Animal('lab', 'Bru', 'soe', 2, 'blue');
 console.log(brusoe);
-
-// Create bird object
-const birdie = new Bird('lab', 'Bru', 'soe', 2, 'blue');
-console.log(birdie);
