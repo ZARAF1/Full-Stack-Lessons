18f3ef70056ae70b2b13bd027bba39f8cf8b7fbc
zaraf1 <ali.raffad@gmail.com>
2023-10-21 11:52:09 +0300

    Prettier formatted text

--- 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/7. Factory Functions.js
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 2/1. Main Lesson 2/7. Factory Functions.js
@@ -1,12 +1,13 @@
 const robotFactory = (model, mobile) => {
-    return {
-        model: model,
-        mobile: mobile,
-        beep: function () {
-            console.log('Beep Bop')
-        }
-    }
-}
-const tinCan = robotFactory('P-500', true)
-tinCan.beep()
-console.log(tinCan)
+  return {
+    model: model,
+    mobile: mobile,
+    beep: function () {
+      console.log('Beep Bop');
+    },
+  };
+};
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
+console.log(tinCan);
+``
