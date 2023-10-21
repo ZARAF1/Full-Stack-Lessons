cbb173816efd085438f9142fe40280596a16cb8e
zaraf1 <ali.raffad@gmail.com>
2023-10-21 18:20:38 +0300

    Added other ways of creating objects

---
+++ 10. JavaScript Syntax, Part II/3. Objects/Lesson 1/1. Creating Objects.js
@@ -0,0 +1,80 @@
+// 1: using the object literal
+
+let spaceShip = {
+  'Fuel Type': 'diesel',
+  color: 'Red',
+  crewNum: 15,
+  intro() {
+    return `This is an intro for spaceship which is riding on the fuel ${this['Fuel Type']}`;
+  },
+};
+console.log({ spaceShip });
+console.log(spaceShip.intro());
+
+// 2: or creating an empty object then assigning it properties and values
+
+let spaceCraft = {};
+spaceCraft.name = 'Trek Star';
+spaceCraft.age = 40;
+spaceCraft.color = 'silver';
+console.log({ spaceCraft });
+
+// 3: Using new keyword:
+// The following example create a new JavaScript object using new Object(), and then adds 4 properties:
+
+let person = new Object();
+person.firstName = 'John';
+person.lastName = 'Doe';
+person.age = 50;
+person.eyeColor = 'blue';
+
+console.log({ person });
+
+// 4: Using a constructor function
+// Constructor function for Person objects
+
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+// constructor function for Animal objects
+function Animal(type, first, last, age, eye) {
+  this.type = type;
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+/**** arrow function syntax doesn't work */
+// const Bird = (type, first, last, age, eye) => {
+//   this.type = type;
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+// };
+
+// function expressions do work
+const Bird = function (type, first, last, age, eye) {
+  this.type = type;
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+};
+
+// Create a Person object
+const myFather = new Person('John', 'Doe', 50, 'blue');
+console.log(myFather);
+console.log(myFather.firstName);
+
+// Create a Animal object
+const brusoe = new Animal('lab', 'Bru', 'soe', 2, 'blue');
+console.log(brusoe);
+
+// Create bird object
+const birdie = new Bird('lab', 'Bru', 'soe', 2, 'blue');
+console.log(birdie);
