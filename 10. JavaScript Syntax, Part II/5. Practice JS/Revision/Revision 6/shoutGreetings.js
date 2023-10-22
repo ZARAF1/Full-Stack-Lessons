1eb06c43801c187bf83a6cd8598c7b759a242001
zaraf1 <ali.raffad@gmail.com>
2023-10-22 13:11:26 +0300

    shout greetings exercise added

---
+++ 10. JavaScript Syntax, Part II/5. Practice JS/Revision/Revision 6/shoutGreetings.js
@@ -0,0 +1,19 @@
+/*
+Write a function shoutGreetings() that takes in an array of strings and returns a new array.
+This new array should contain all the strings from the argument array but with capitalized letters and an
+exclamation point appended to the end: 'heya' will become 'HEYA!'
+
+You can use any technique you want to accomplish this task.
+
+You can test your function when you’re ready by passing in the greetings array or by making your own array!
+    */
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+const shoutGreetings = arr => {
+  let bigLetters = arr.map(element => {
+    return element.toUpperCase() + '!';
+  });
+  return bigLetters;
+};
+console.log(shoutGreetings(greetings));
