f7ca2294d094a2b4d3bd4c2ba93863feca504669
zaraf1 <ali.raffad@gmail.com>
2023-10-22 13:10:56 +0300

    Added convert to baby exercise

---
+++ 10. JavaScript Syntax, Part II/5. Practice JS/Revision/Revision 6/convertToBaby.js
@@ -0,0 +1,23 @@
+/*
+convertToBaby()
+Instructions
+1.
+Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with
+each string in the array prepended with 'baby '.
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+convertToBaby(animals);
+// Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
+Note: You may have noticed how convenient it would be to use map, but you’re not allowed to use it here. Don’t worry
+you’ll have plenty of opportunities to use built-in methods later on!*/
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+const convertToBaby = arr => {
+  let babyAnimals = [];
+  for (let i = 0; i < arr.length; i++) {
+    babyAnimals.push(`Baby ${arr[i]}`);
+  }
+  return babyAnimals;
+};
+console.log(convertToBaby(animals));
