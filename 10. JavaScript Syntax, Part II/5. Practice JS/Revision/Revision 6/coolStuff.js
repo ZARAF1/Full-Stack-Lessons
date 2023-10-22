fe426210cd4d955d845274d496867a18b6dccc17
zaraf1 <ali.raffad@gmail.com>
2023-10-22 13:51:42 +0300

    using filter and includes array methods for testing if two arrays have same items

---
+++ 10. JavaScript Syntax, Part II/5. Practice JS/Revision/Revision 6/coolStuff.js
@@ -0,0 +1,19 @@
+/*Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in
+.filter() method, returns an array with the items that are present in both arrays.
+
+let arr1 = ['this', 'not this', 'nor this'];
+let arr2 = ['thing 1', 'thing 2', 'this'];
+
+justCoolStuff(arr1, arr2); // Should return ['this']
+You can test your function when you’re ready by passing in the myStuff and coolStuff arrays or by making arrays of
+ your own!*/
+
+// Write your code here:
+let arr1 = ['this', 'not this', 'nor this'];
+let arr2 = ['thing 1', 'thing 2', 'this'];
+const justCoolStuff = (arr1, arr2) => {
+  let sameElements = arr1.filter(element => arr2.includes(element));
+  return sameElements;
+};
+
+console.log(justCoolStuff(arr1, arr2));
